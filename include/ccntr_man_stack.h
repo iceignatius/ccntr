746508bf8cae910bfc26771c1ba41bc7a8a15d83
Ignatius <ice.emissary@gmail.com>
2017-03-23 07:33:02 +0800

    修改 managed stack，將部份介面函式變更為 inline 型態。

--- include/ccntr_man_stack.h
+++ include/ccntr_man_stack.h
@@ -40,10 +40,34 @@ typedef struct ccntr_man_stack_t
 void ccntr_man_stack_init(ccntr_man_stack_t *self, ccntr_man_stack_release_value_t release_value);
 void ccntr_man_stack_destroy(ccntr_man_stack_t *self);
 
-unsigned ccntr_man_stack_get_count(const ccntr_man_stack_t *self);
+static inline
+unsigned ccntr_man_stack_get_count(const ccntr_man_stack_t *self)
+{
+    /**
+     * @memberof ccntr_man_stack_t
+     * @brief Get count of values it contained.
+     *
+     * @param self Object instance.
+     * @return The count of values.
+     */
+    return ccntr_stack_get_count(&self->super);
+}
 
 void* ccntr_man_stack_get_current(ccntr_man_stack_t *self);
-const void* ccntr_man_stack_get_current_c(const ccntr_man_stack_t *self);
+
+static inline
+const void* ccntr_man_stack_get_current_c(const ccntr_man_stack_t *self)
+{
+    /**
+     * @memberof ccntr_man_stack_t
+     * @brief Get the current value.
+     *
+     * @param self Object instance.
+     * @return The current value;
+     *         or NULL if container is empty.
+     */
+    return ccntr_man_stack_get_current((ccntr_man_stack_t*)self);
+}
 
 void ccntr_man_stack_push(ccntr_man_stack_t *self, void *value);
 void* ccntr_man_stack_pop(ccntr_man_stack_t *self);
--- src/ccntr_man_stack.c
+++ src/ccntr_man_stack.c
@@ -81,18 +81,6 @@ void ccntr_man_stack_destroy(ccntr_man_stack_t *self)
     ccntr_man_stack_clear(self);
 }
 //------------------------------------------------------------------------------
-unsigned ccntr_man_stack_get_count(const ccntr_man_stack_t *self)
-{
-    /**
-     * @memberof ccntr_man_stack_t
-     * @brief Get count of values it contained.
-     *
-     * @param self Object instance.
-     * @return The count of values.
-     */
-    return ccntr_stack_get_count(&self->super);
-}
-//------------------------------------------------------------------------------
 void* ccntr_man_stack_get_current(ccntr_man_stack_t *self)
 {
     /**
@@ -108,19 +96,6 @@ void* ccntr_man_stack_get_current(ccntr_man_stack_t *self)
 
     element_t *ele = container_of(node, element_t, node);
     return ele->value;
-}
-//------------------------------------------------------------------------------
-const void* ccntr_man_stack_get_current_c(const ccntr_man_stack_t *self)
-{
-    /**
-     * @memberof ccntr_man_stack_t
-     * @brief Get the current value.
-     *
-     * @param self Object instance.
-     * @return The current value;
-     *         or NULL if container is empty.
-     */
-    return ccntr_man_stack_get_current((ccntr_man_stack_t*)self);
 }
 //------------------------------------------------------------------------------
 void ccntr_man_stack_push(ccntr_man_stack_t *self, void *value)
